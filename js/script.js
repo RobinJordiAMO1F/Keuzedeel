f70e58864a0cb56afe4980d86d3674628e6f64fe
jordileenaars <jordileenaars@gmail.com>
2018-10-24 09:20:56 +0200

    js 2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 let button = document.getElementById('rotateBtn');
 let	body = document.querySelector('body');
 
+
+
 button.addEventListener('dblclick', () => {
 	body.classList.add('rotate')
+
 })
+
+let button = document.getElementById('rotate');
+
+	button.addEventListener('click', () => {
+		body.classList.add('rotate')
+	})
